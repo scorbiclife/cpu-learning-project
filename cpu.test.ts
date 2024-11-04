6ec12794639c4e4ccd185c1aa57f30aac0b0484e
scorbiclife <scorbiclife+git@gmail.com>
2024-11-04 09:19:31 +0900

    feat: immediate and direct load and store

--- cpu.test.ts
+++ cpu.test.ts
@@ -6,16 +6,16 @@ const { R1, R2 } = Register;
 const { LOAD_DIRECT, LOAD_IMMEDIATE, STORE_DIRECT, NOP, ADD } = Opcode;
 
 test("can fetch instructions of opcode + register + 2 byte data", () => {
+    const memory: Byte[] = Array(65536).fill(0x00);
     // prettier-ignore
     const program: Byte[] = [
-        LOAD_DIRECT, R1, 0x00, 0x01,
-        LOAD_IMMEDIATE, R2, 0x00, 0x02,
+        LOAD_DIRECT, R1, 0x00, 0x01, // Offset 4096
+        LOAD_IMMEDIATE, R2, 0x02, 0x00,
         NOP, 0x00, 0x00, 0x00,
         ADD, R1, R2, 0x00,
         STORE_DIRECT, R1, 0x00, 0x01,
     ];
-    const stack = Array(1024).fill(0x00);
-    const memory = ([] as Byte[]).concat(program, stack);
+    program.forEach((byte, i) => memory[i] = byte);
     const cpu = new Cpu(memory);
     const instructions: Word[] = [];
     while (cpu.programCounter < program.length) {
@@ -29,4 +29,26 @@ test("can fetch instructions of opcode + register + 2 byte data", () => {
         ADD,
         STORE_DIRECT,
     ]);
+});
+
+test("can fetch instructions of opcode + register + 2 byte data", () => {
+    const memory: Byte[] = Array(65536).fill(0x00);
+    memory[4096] = 0x00;
+    memory[4097] = 0x10;
+    // prettier-ignore
+    const program: Byte[] = [
+        LOAD_DIRECT, R1, 0x00, 0x01, // Offset 4096
+        LOAD_IMMEDIATE, R2, 0x02, 0x00,
+        NOP, 0x00, 0x00, 0x00,
+        ADD, R1, R2, 0x00,
+        STORE_DIRECT, R1, 0x00, 0x01,
+    ];
+    program.forEach((byte, i) => memory[i] = byte);
+    const cpu = new Cpu(memory);
+    while (cpu.programCounter < program.length) {
+        cpu.handleInstruction();
+    }
+    expect(memory[4097]).toBe(0x10);
+    // TODO: implement add
+    // expect(memory[4096]).toBe(0x02);
 });
--- cpu.ts
+++ cpu.ts
@@ -1,4 +1,4 @@
-import { Word, Byte, getWordFromBytes } from "./lib";
+import { Word, Byte, getWordFromBytes, getBytesFromWord } from "./lib";
 
 export enum Register {
     START,
@@ -17,7 +17,6 @@ export enum Opcode {
     START,
     NOP = START,
     LOAD_IMMEDIATE,
-    STORE_IMMEDIATE,
     LOAD_DIRECT,
     STORE_DIRECT,
     MOV,
@@ -43,18 +42,38 @@ export class Cpu {
     programCounter: Word;
     memoryAddressRegister: Word;
     memoryBufferRegister: Word;
-    memoryBufferRegister2: Word;
     instructionRegister: Word;
-    flagsResister: FlagsRegister;
+    flagsRegister: FlagsRegister;
     generalRegisters: Record<Register, Word>;
     stackPointer: Word;
     basePointer: Word;
 
     // possibly different from real cpus
+    registerOperand: Register;
+    dataOperand: Word;
     nextProgramCounter: Word;
 
     constructor(public memory: Byte[]) {
         this.programCounter = 0;
+        this.flagsRegister = {
+            sign: false,
+            zero: false,
+            carry: false,
+            overflow: false,
+            interrupt: false,
+            supervisor: false,
+        };
+        this.generalRegisters = {
+            [Register.R0]: 0x0000,
+            [Register.R1]: 0x0000,
+            [Register.R2]: 0x0000,
+            [Register.R3]: 0x0000,
+            [Register.R4]: 0x0000,
+            [Register.R5]: 0x0000,
+            [Register.R6]: 0x0000,
+            [Register.R7]: 0x0000,
+            [Register.END]: 0x0000, // unused
+        };
         this.stackPointer = memory.length - 1;
         this.basePointer = memory.length - 1;
     }
@@ -65,12 +84,22 @@ export class Cpu {
         this.updateProgramCounter();
     }
 
-    fetchByte(address: number) {
+    /*
+     * 메모리 읽기 및 쓰기는 무조건 mar, mbr을 거쳐 진행하도록 만들었습니다.
+     */
+
+    loadByte(address: Word) {
         this.memoryAddressRegister = address;
         this.memoryBufferRegister = this.memory[this.memoryAddressRegister];
     }
 
-    fetchWord(address: number) {
+    storeByte(address: Word, value: Byte) {
+        this.memoryAddressRegister = address;
+        this.memoryBufferRegister = value;
+        this.memory[this.memoryAddressRegister] = this.memoryBufferRegister;
+    }
+
+    loadWord(address: Word) {
         this.memoryAddressRegister = address;
         this.memoryBufferRegister = getWordFromBytes(
             this.memory[this.memoryAddressRegister],
@@ -78,17 +107,21 @@ export class Cpu {
         );
     }
 
-    fetchDoubleWord(address: number) {
-        this.fetchWord(address);
-        this.memoryBufferRegister2 = getWordFromBytes(
-            this.memory[this.memoryAddressRegister + 2],
-            this.memory[this.memoryAddressRegister + 3]
-        )
+    storeWord(address: Word, value: Word) {
+        this.memoryAddressRegister = address;
+        this.memoryBufferRegister = value;
+        const [byte0, byte1] = getBytesFromWord(this.memoryBufferRegister);
+        this.memory[this.memoryAddressRegister] = byte0;
+        this.memory[this.memoryAddressRegister + 1] = byte1;
     }
 
     fetch() {
-        this.fetchByte(this.programCounter);
+        this.loadByte(this.programCounter);
         this.instructionRegister = this.memoryBufferRegister;
+        this.loadByte(this.programCounter + 1);
+        this.registerOperand = this.memoryBufferRegister;
+        this.loadWord(this.programCounter + 2);
+        this.dataOperand = this.memoryBufferRegister;
         this.nextProgramCounter = this.programCounter + 4;
     }
 
@@ -98,15 +131,23 @@ export class Cpu {
                 return;
             }
             case Opcode.LOAD_IMMEDIATE: {
-                // decode
-            }
-            case Opcode.STORE_IMMEDIATE: {
-                break;
+                // write-back
+                this.generalRegisters[this.registerOperand] = this.dataOperand;
             }
             case Opcode.LOAD_DIRECT: {
+                // memory
+                this.loadWord(this.dataOperand);
+                // write-back
+                this.generalRegisters[this.registerOperand] =
+                    this.memoryBufferRegister;
                 break;
             }
             case Opcode.STORE_DIRECT: {
+                // memory
+                this.storeWord(
+                    this.dataOperand,
+                    this.generalRegisters[this.registerOperand]
+                );
                 break;
             }
             case Opcode.MOV: {
