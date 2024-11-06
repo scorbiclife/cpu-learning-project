162a9f939176f6fd729a871ee22e5b5d4ee19577
scorbiclife <scorbiclife+git@gmail.com>
2024-11-06 09:34:03 +0900

    refactor: separate control unit
    control unit은 CPU를 constructor로 가짐
    
    Control Unit과 다른 모듈들이 tightly coupled 되어 있어서
    CU 관련 내용들을 모듈화하되 CPU와 독립적으로 만들지는 않기로 하였다.

--- cpu.ts
+++ cpu.ts
@@ -38,23 +38,87 @@ type FlagsRegister = {
     supervisor: boolean;
 };
 
-export class Cpu {
+/**
+ * 참고 자료: https://esyeonge.tistory.com/28
+ */
+export class CpuControlUnit {
     programCounter: Word;
     memoryAddressRegister: Word;
     memoryBufferRegister: Word;
     instructionRegister: Word;
+
+    registerOperand: Byte;
+    dataOperand: Word;
+
+    constructor(private cpu: Cpu, programCounter: Word) {
+        this.programCounter = programCounter;
+    }
+
+    loadByte(address: Word) {
+        this.memoryAddressRegister = address;
+        this.memoryBufferRegister = this.cpu.memory[this.memoryAddressRegister];
+    }
+
+    storeByte(address: Word, value: Byte) {
+        this.memoryAddressRegister = address;
+        this.memoryBufferRegister = value;
+        this.cpu.memory[this.memoryAddressRegister] = this.memoryBufferRegister;
+    }
+
+    loadWord(address: Word) {
+        this.memoryAddressRegister = address;
+        this.memoryBufferRegister = getWordFromBytes(
+            this.cpu.memory[this.memoryAddressRegister],
+            this.cpu.memory[this.memoryAddressRegister + 1]
+        );
+    }
+
+    storeWord(address: Word, value: Word) {
+        this.memoryAddressRegister = address;
+        this.memoryBufferRegister = value;
+        const [byte0, byte1] = getBytesFromWord(this.memoryBufferRegister);
+        this.cpu.memory[this.memoryAddressRegister] = byte0;
+        this.cpu.memory[this.memoryAddressRegister + 1] = byte1;
+    }
+
+    fetch() {
+        this.loadByte(this.programCounter);
+        this.instructionRegister = this.memoryBufferRegister;
+        this.loadByte(this.programCounter + 1);
+        this.registerOperand = this.memoryBufferRegister;
+        this.loadWord(this.programCounter + 2);
+        this.dataOperand = this.memoryBufferRegister;
+        this.programCounter += 4;
+    }
+
+}
+
+export class Cpu {
+    controlUnit: CpuControlUnit;
+
+    get programCounter(): Word {
+        return this.controlUnit.programCounter;
+    }
+
+    get memoryAddressRegister(): Word {
+        return this.controlUnit.memoryAddressRegister;
+    }
+
+    get memoryBufferRegister(): Word {
+        return this.controlUnit.memoryBufferRegister;
+    }
+
+    get instructionRegister(): Word {
+        return this.controlUnit.instructionRegister;
+    }
+
     flagsRegister: FlagsRegister;
     generalRegisters: Record<Register, Word>;
     stackPointer: Word;
     baseRegister: Word;
 
-    // possibly different from real cpus
-    registerOperand: Register;
-    dataOperand: Word;
-    nextProgramCounter: Word;
-
     constructor(public memory: Byte[]) {
-        this.programCounter = 0;
+        this.controlUnit = new CpuControlUnit(this, 0x0000);
         this.flagsRegister = {
             sign: false,
             zero: false,
@@ -79,75 +143,34 @@ export class Cpu {
     }
 
     handleInstruction() {
-        this.fetch();
+        this.controlUnit.fetch();
         this.decodeExecuteWriteback();
-        this.updateProgramCounter();
-    }
-
-    /*
-     * 메모리 읽기 및 쓰기는 무조건 mar, mbr을 거쳐 진행하도록 만들었습니다.
-     */
-
-    loadByte(address: Word) {
-        this.memoryAddressRegister = address;
-        this.memoryBufferRegister = this.memory[this.memoryAddressRegister];
-    }
-
-    storeByte(address: Word, value: Byte) {
-        this.memoryAddressRegister = address;
-        this.memoryBufferRegister = value;
-        this.memory[this.memoryAddressRegister] = this.memoryBufferRegister;
-    }
-
-    loadWord(address: Word) {
-        this.memoryAddressRegister = address;
-        this.memoryBufferRegister = getWordFromBytes(
-            this.memory[this.memoryAddressRegister],
-            this.memory[this.memoryAddressRegister + 1]
-        );
-    }
-
-    storeWord(address: Word, value: Word) {
-        this.memoryAddressRegister = address;
-        this.memoryBufferRegister = value;
-        const [byte0, byte1] = getBytesFromWord(this.memoryBufferRegister);
-        this.memory[this.memoryAddressRegister] = byte0;
-        this.memory[this.memoryAddressRegister + 1] = byte1;
-    }
-
-    fetch() {
-        this.loadByte(this.programCounter);
-        this.instructionRegister = this.memoryBufferRegister;
-        this.loadByte(this.programCounter + 1);
-        this.registerOperand = this.memoryBufferRegister;
-        this.loadWord(this.programCounter + 2);
-        this.dataOperand = this.memoryBufferRegister;
-        this.nextProgramCounter = this.programCounter + 4;
     }
 
     decodeExecuteWriteback() {
-        switch (this.instructionRegister) {
+        switch (this.controlUnit.instructionRegister) {
             case Opcode.NOP: {
                 return;
             }
             case Opcode.LOAD_IMMEDIATE: {
                 // write-back
-                this.generalRegisters[this.registerOperand] = this.dataOperand;
+                this.generalRegisters[this.controlUnit.registerOperand] =
+                    this.controlUnit.dataOperand;
                 return;
             }
             case Opcode.LOAD_DIRECT: {
                 // memory
-                this.loadWord(this.dataOperand);
+                this.controlUnit.loadWord(this.controlUnit.dataOperand);
                 // write-back
-                this.generalRegisters[this.registerOperand] =
-                    this.memoryBufferRegister;
+                this.generalRegisters[this.controlUnit.registerOperand] =
+                    this.controlUnit.memoryBufferRegister;
                 return;
             }
             case Opcode.STORE_DIRECT: {
                 // memory
-                this.storeWord(
-                    this.dataOperand,
-                    this.generalRegisters[this.registerOperand]
+                this.controlUnit.storeWord(
+                    this.controlUnit.dataOperand,
+                    this.generalRegisters[this.controlUnit.registerOperand]
                 );
                 return;
             }
@@ -173,9 +196,5 @@ export class Cpu {
                 return;
             }
         }
-    }
-
-    updateProgramCounter() {
-        this.programCounter = this.nextProgramCounter;
     }
 }
